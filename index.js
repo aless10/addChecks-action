c9bfc0a39d4a1d560a5970b0e8e361f3ba6ab995
aless10 <alessio.izzo86@gmail.com>
2020-12-15 23:23:26 +0100

    start working on it

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const core = require('@actions/core');
-const wait = require('./wait');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
 
     core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
+    console.log("yeeeee")
     core.info((new Date()).toTimeString());
 
     core.setOutput('time', new Date().toTimeString());
@@ -18,4 +15,8 @@ async function run() {
   }
 }
 
-run();
+const checks = core.getInput('checks');
+
+for (let i = 0; i < checks; i++) {
+  run();
+}
