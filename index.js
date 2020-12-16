e0f15af874d0781110e9ac6c9fa120a3987e9379
aless10 <alessio.izzo86@gmail.com>
2020-12-16 23:53:08 +0100

    adding single files for pre and post

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ const wait = require('./wait');
 // most @actions toolkit packages have async methods
 async function run(i) {
   try {
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     await wait(parseInt(i) * 100);
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
   }
